d43a80dd546d2a73adc11e5703ce5ab228908f11
Mrudula Math <mrudula.math@microfocus.com>
2021-05-03 20:55:44 +0530

    first commit

---
+++ data/products.js
@@ -0,0 +1,62 @@
+var products =[
+    {
+      "image": "images/formal shirt - men.jfif",
+      "description": "Formal shirt for Men",
+      "category": "Men",
+      "price": "599"
+    },
+    {
+      "image": "images/casual shirt - men.jfif",
+      "description": "Casual shirt for Men",
+      "category": "Men",
+      "price": "599"
+    },
+    {
+      "image": "images/round neck tshirt - men.jfif",
+      "description": "Round neck T-shirt for Men",
+      "category": "Men",
+      "price": "599"
+    },
+    {
+      "image": "images/jacket men.jfif",
+      "description": "Jacket for Men",
+      "category": "Men",
+      "price": "599"
+    },
+    {
+      "image": "images/kurta - men.jfif",
+      "description": "Kurta for Men",
+      "category": "Men",
+      "price": "599"
+    },
+    {
+      "image": "images/formal shirt women.jfif",
+      "description": "Formal shirt for Women",
+      "category": "Women",
+      "price": "599"
+    },
+    {
+      "image": "images/casual top women.jfif",
+      "description": "Casual top for Women",
+      "category": "Women",
+      "price": "599"
+    },
+    {
+      "image": "images/kurta - women.jfif",
+      "description": "Kurta for Women",
+      "category": "Women",
+      "price": "599"
+    },
+    {
+      "image": "images/saree.jfif",
+      "description": "Saree for Women",
+      "category": "Women",
+      "price": "599"
+    },
+    {
+      "image": "images/western dress.jpg",
+      "description": "Western for Women",
+      "category": "Women",
+      "price": "599"
+    }
+  ]
